958c7260a089dd524c01a8a61f89f506fda83eb2
palak <palaksdarji@gmail.com>
2020-06-11 16:19:43 +0530

    firebase messaging integrated

--- app/src/main/java/com/palak/solarimportexporttracker/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/palak/solarimportexporttracker/ui/activities/HomeActivity.kt
@@ -1,7 +1,14 @@
 package com.palak.solarimportexporttracker.ui.activities
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import com.palak.solarimportexporttracker.R
 
 /**
@@ -14,5 +21,40 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        setupNotification()
+    }
+
+    private fun setupNotification(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create channel to show notifications.
+            val channelId = getString(R.string.default_notification_channel_id)
+            val channelName = getString(R.string.default_notification_channel_name)
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(
+                NotificationChannel(channelId,
+                channelName, NotificationManager.IMPORTANCE_LOW)
+            )
+        }
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = getString(R.string.msg_token_fmt, token)
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_LONG).show()
+            })
+    }
+
+    companion object {
+        private const val TAG = "HomeActivity"
     }
 }
